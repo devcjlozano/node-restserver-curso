6021410bb6aa7c6e7ed66b94546c17584728286e
cjlozano <carlosjlp@gmail.com>
2020-08-03 17:27:52 +0200

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config')
 
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 const bodyParser = require('body-parser')
@@ -9,34 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //parse aplication/json
 app.use(bodyParser.json())
 
-app.get('/usuario', (req, res) => {
-  res.json('get Usuario')
-})
-app.post('/usuario', (req, res) => {
-  const body = req.body
-   
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    })
-  } else {
+app.use(require('./routes/usuario'))
 
-  }
-  res.json({
-    body
-  })
-})
-app.put('/usuario/:id', (req, res) => {
-  const id = req.params.id
-  res.json({
-    id
-  })
-})
-app.delete('/usuario', (req, res) => {
-  res.json('delete Usuario')
-})
 
 app.listen(process.env.PORT, () => {
   console.log('Escuchando puerto: ', process.env.PORT)
+})
+
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true}, (err, resp) => {
+  if(err) throw err
+    console.log('Base de datos ONLINE')
 })
